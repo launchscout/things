76bc7a67cca6fb2a1fc2412640d6ccd164771403
Chris Nelson <superchrisnelson@gmail.com>
2015-11-24 09:45:16 -0500

    Things are happening

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
-import { bootstrap, Component } from "angular2";
+import { bootstrap, Component, CORE_DIRECTIVES } from "angular2";
 
 @Component({
-  template: "Hi there it worked!!!",
-  selector: "app"
+  templateUrl: "app/app.html",
+  selector: "app",
+  directives: [CORE_DIRECTIVES]
 })
 class App {
-
+  constructor() {
+    this.things = [{id: 1, name: "Thing one"}, {id: 2, name: "Thing two"}];
+  }
 }
 
 bootstrap(App, []);
